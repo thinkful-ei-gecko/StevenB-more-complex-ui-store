aa58ccc66aa778e8cb8d198729df94cc32fff3a6
Steven Bull <stevenwbulljr@gmail.com>
2019-08-20 14:00:23 -0500

    updated html to include search text box and an edit button

--- index.js
+++ index.js
@@ -1,11 +1,14 @@
 'use strict';
+// eslint-disable-next-line no-unused-vars
+/* global var, cuid, ? */
+
 
 const STORE = {
   items: [
-    {id: cuid(), name: "apples", checked: false},
-    {id: cuid(), name: "oranges", checked: false},
-    {id: cuid(), name: "milk", checked: true},
-    {id: cuid(), name: "bread", checked: false}
+    {id: cuid(), name: 'apples', checked: false},
+    {id: cuid(), name: 'oranges', checked: false},
+    {id: cuid(), name: 'milk', checked: true},
+    {id: cuid(), name: 'bread', checked: false}
   ],
   hideCompleted: false
 };
@@ -13,8 +16,11 @@ const STORE = {
 function generateItemElement(item) {
   return `
     <li data-item-id="${item.id}">
-      <span class="shopping-item js-shopping-item ${item.checked ? "shopping-item__checked" : ''}">${item.name}</span>
+      <span class="shopping-item js-shopping-item ${item.checked ? 'shopping-item__checked' : ''}">${item.name}</span>
       <div class="shopping-item-controls">
+        <button class="shopping-item-edit js-item-edit">
+          <span class="button-label">edit</span>
+        </button>
         <button class="shopping-item-toggle js-item-toggle">
             <span class="button-label">check</span>
         </button>
@@ -27,11 +33,11 @@ function generateItemElement(item) {
 
 
 function generateShoppingItemsString(shoppingList) {
-  console.log("Generating shopping list element");
+  console.log('Generating shopping list element');
 
   const items = shoppingList.map((item) => generateItemElement(item));
   
-  return items.join("");
+  return items.join('');
 }
 
 
@@ -75,7 +81,7 @@ function handleNewItemSubmit() {
 }
 
 function toggleCheckedForListItem(itemId) {
-  console.log("Toggling checked property for item with id " + itemId);
+  console.log('Toggling checked property for item with id ' + itemId);
   const item = STORE.items.find(item => item.id === itemId);
   item.checked = !item.checked;
 }
@@ -88,7 +94,7 @@ function getItemIdFromElement(item) {
 }
 
 function handleItemCheckClicked() {
-  $('.js-shopping-list').on('click', `.js-item-toggle`, event => {
+  $('.js-shopping-list').on('click', '.js-item-toggle', event => {
     console.log('`handleItemCheckClicked` ran');
     const id = getItemIdFromElement(event.currentTarget);
     toggleCheckedForListItem(id);
@@ -99,7 +105,7 @@ function handleItemCheckClicked() {
 
 // name says it all. responsible for deleting a list item.
 function deleteListItem(itemId) {
-  console.log(`Deleting item with id  ${itemId} from shopping list`)
+  console.log(`Deleting item with id  ${itemId} from shopping list`);
 
   // as with `addItemToShoppingLIst`, this function also has the side effect of
   // mutating the global STORE value.
